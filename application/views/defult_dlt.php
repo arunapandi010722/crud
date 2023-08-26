9748796511c21b64325e925dbd6ab3512fb1f5c5
Aruna <arunapandithivi@gmail.com>
2023-08-26 06:46:19 +0530

    updated

---
+++ application/views/defult_dlt.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>delete</title>
+</head>
+<body>
+    <?php 
+    echo "<h1> successfully deleted <h1>";
+    ?>
+    <button> <a href="<?=base_url('register/fetchdata')?>">Go to Table</a></button>
+    <button> <a href="<?=base_url('register/registeration_form')?>">Go to Form</a></button>
+</body>
+</html>
